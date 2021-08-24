c311a3295835a090de4062895fd51a1cac1d5aeb
Alexey Tretyakov <gggvnr@gmail.com>
2021-08-24 17:56:04 +0500

    feat: all articles metadata now fetching at ssr phase

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -5,6 +5,7 @@ import PageHead from '../../components/PageHead/PageHead';
 import Articles from '../../partials/Articles/Articles';
 
 import { fetchArticlesList } from '../../partials/Articles/fetchHelpers/fetchArticlesList';
+import { fetchMetadata } from '../../partials/Articles/fetchHelpers/fetchMetadata';
 
 export default function ArticlesPage({
   articles,
@@ -34,10 +35,24 @@ export default function ArticlesPage({
 export async function getStaticProps({ locale }) {
   const articles = await fetchArticlesList();
 
+  const localeArticles = articles[locale];
+
+  const currentArticles = await Promise.all(localeArticles.map(async (articleFolder) => {
+    const files = articleFolder.children;
+    const articleFilename = files.filter((file) => file.name !== 'metadata.json')[0].name;
+
+    const metadata = await fetchMetadata(articleFilename, locale);
+
+    return {
+      ...articleFolder,
+      metadata,
+    };
+  }));
+
   return {
     props: {
       ...(await serverSideTranslations(locale)),
-      articles,
+      articles: currentArticles,
     },
   };
 }
--- partials/Articles/Articles.js
+++ partials/Articles/Articles.js
@@ -1,8 +1,5 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-
-import { fetchMetadata } from './fetchHelpers/fetchMetadata';
 
 export default function Articles({
   articles,
@@ -12,7 +9,7 @@ export default function Articles({
   return (
     <div className="container articles">
       <ul className="articles-list">
-        {articles[router.locale].map((folder) => (
+        {articles.map((folder) => (
           <li key={folder.name} className="articles__item">
             <ArticleLink articleFolder={folder} router={router} locale={router.locale} />
           </li>
@@ -30,30 +27,18 @@ function ArticleLink({
   const articleFilename = files.filter((file) => file.name !== 'metadata.json')[0].name;
   const availableLocales = router.locales.filter((locale) => locale !== router.locale);
 
-  const [meta, setMeta] = useState({});
-
-  useEffect(() => {
-    async function loadMeta() {
-      const metadata = await fetchMetadata(articleFilename, router.locale);
-
-      setMeta(metadata);
-    }
-
-    loadMeta();
-  }, [articleFilename, router.locale]);
-
   return (
     <>
       <Link href={`/articles/${articleFilename}`} locale={router.locale}>
         <a className="articles__link">
-          {meta.title}
+          {articleFolder.metadata.title}
         </a>
       </Link>
 
       {' '}
       (
       {availableLocales.map((locale) => (
-        <Link href={`/articles/${articleFilename}`} locale={locale}>
+        <Link key={locale} href={`/articles/${articleFilename}`} locale={locale}>
           <a className="articles__link">
             {locale.toUpperCase()}
           </a>
