5b6e1f7ef8f235cfb6250bfba167459274c2057b
Alexey Tretyakov <gggvnr@gmail.com>
2021-08-24 17:41:13 +0500

    feat: add articles secondary lang links

--- partials/Articles/Articles.js
+++ partials/Articles/Articles.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 import { fetchMetadata } from './fetchHelpers/fetchMetadata';
@@ -6,45 +7,59 @@ import { fetchMetadata } from './fetchHelpers/fetchMetadata';
 export default function Articles({
   articles,
 }) {
+  const router = useRouter();
+
   return (
     <div className="container articles">
-      {Object.keys(articles).map((locale) => (
-        <ul key={locale} className="articles-list">
-          {articles[locale].map((folder) => (
-            <li key={folder.name} className="articles__item">
-              <ArticleLink articleFolder={folder} locale={locale} />
-            </li>
-          ))}
-        </ul>
-      ))}
+      <ul className="articles-list">
+        {articles[router.locale].map((folder) => (
+          <li key={folder.name} className="articles__item">
+            <ArticleLink articleFolder={folder} router={router} locale={router.locale} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 function ArticleLink({
   articleFolder = {},
-  locale = '',
+  router,
 }) {
   const files = articleFolder.children;
   const articleFilename = files.filter((file) => file.name !== 'metadata.json')[0].name;
+  const availableLocales = router.locales.filter((locale) => locale !== router.locale);
 
   const [meta, setMeta] = useState({});
 
   useEffect(() => {
     async function loadMeta() {
-      const metadata = await fetchMetadata(articleFilename, locale);
+      const metadata = await fetchMetadata(articleFilename, router.locale);
 
       setMeta(metadata);
     }
 
     loadMeta();
-  }, [articleFilename, locale]);
+  }, [articleFilename, router.locale]);
 
   return (
-    <Link href={`/articles/${articleFilename}`} locale={locale}>
-      <a className="articles__link">
-        {meta.title}
-      </a>
-    </Link>
+    <>
+      <Link href={`/articles/${articleFilename}`} locale={router.locale}>
+        <a className="articles__link">
+          {meta.title}
+        </a>
+      </Link>
+
+      {' '}
+      (
+      {availableLocales.map((locale) => (
+        <Link href={`/articles/${articleFilename}`} locale={locale}>
+          <a className="articles__link">
+            {locale.toUpperCase()}
+          </a>
+        </Link>
+      ))}
+      )
+    </>
   );
 }
