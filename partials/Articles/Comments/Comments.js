24fb3462d538e2d7e98c1edd77925270db8931e1
Alexey Tretyakov <gggvnr@gmail.com>
2021-09-03 18:37:00 +0500

    chore: update comments oauth

--- partials/Articles/Comments/Comments.js
+++ partials/Articles/Comments/Comments.js
@@ -7,8 +7,8 @@ export default function Comments() {
   return (
     <GitalkComponent
       options={{
-        clientID: 'dc32e02e5f1ab0fa88dd',
-        clientSecret: 'e45f64b28696c957081378453ea33cee8ef6e513',
+        clientID: '579b35685670b2880de2',
+        clientSecret: '8544bfd6cf80d7eb6c58931517644cad4e03b7a8',
         repo: 'TourmalineCore.Articles',
         owner: 'TourmalineCore',
         admin: ['fpandyz', 'gggvnr'],
