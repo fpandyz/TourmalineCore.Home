01605d6b51dc5df3de20040678ce90989cb53494
Yasnovsky <andrey5.1997@mail.ru>
2022-07-29 17:28:25 +0500

    fix(hero block): #2tynw70: fix name key for localization and style, structure file

--- components/HeroBlock/HeroBlock.tsx
+++ components/HeroBlock/HeroBlock.tsx
@@ -4,21 +4,23 @@ function HeroBlock() {
   const { t } = useTranslation('heroBlock');
 
   return (
-    <section className="container section hero-block">
-      <div className="hero-block__inner">
-        <h1 className="title-type-1">
-          {t('title')}
-          <span className="hero-block__gradient">{t('title-gradient')}</span>
-        </h1>
-        <div className="title-type-4 hero-block__description">{t('description')}</div>
+    <section className="section hero-block">
+      <div className="container hero-block__wrapper">
+        <div className="hero-block__inner">
+          <h1 className="title-type-1">
+            {t('title')}
+            <span className="hero-block__gradient-title">{t('gradient-title')}</span>
+          </h1>
+          <div className="title-type-4 hero-block__description">{t('description')}</div>
 
-        <div>Кнопка</div>
-      </div>
-      <div className="hero-block__video">
-        <video src="/images/video.mp4" autoPlay loop playsInline muted>
-          <source src="video.mp4" type="video/mp4" />
-          <track kind="captions" />
-        </video>
+          <div>Кнопка</div>
+        </div>
+        <div className="hero-block__video">
+          <video src="/images/video.mp4" autoPlay loop playsInline muted>
+            <source src="video.mp4" type="video/mp4" />
+            <track kind="captions" />
+          </video>
+        </div>
       </div>
     </section>
   );
