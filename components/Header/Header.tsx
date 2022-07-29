38cb85626762dca096152f89408ae7e908d08bdf
Maksim Valiakine <mvaliakine@tourmalinecore.com>
2022-07-29 15:03:10 +0500

    feat(header): #2t7uk1g: add header and mobile header, add footer links component

---
+++ components/Header/Header.tsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import Link from 'next/link';
+import { useTranslation } from 'next-i18next';
+
+import LangSwitch from '../LangSwitch/LangSwitch';
+import MobileMenu from '../MobileMenu/MobileMenu';
+
+import IconLogo from '../../icons/logo.svg';
+import IconBurger from '../../icons/burger.svg';
+
+type HeaderLinks = {
+  id: string;
+  link: string;
+}[];
+
+const headerLinks: HeaderLinks = [
+  {
+    id: 'ourCompany',
+    link: '/',
+  },
+  {
+    id: 'articles',
+    link: '/articles',
+  },
+];
+
+function Header() {
+  const { t } = useTranslation('common');
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(true);
+
+  return (
+    <>
+      <header className="header">
+        <div className="container header__inner">
+          <Link href="/">
+            <a className="header__logo">
+              <IconLogo />
+            </a>
+          </Link>
+
+          <div className="header__right-panel">
+            <button type="button" className="header__menu-toggle" onClick={() => setIsMobileMenuOpen(true)}>
+              <IconBurger />
+            </button>
+
+            <div className="header__desktop body-type-2">
+              {headerLinks.map((headerLink) => (
+                <Link key={headerLink.id} href={headerLink.link}>
+                  <a className="header__link">
+                    {t(headerLink.id)}
+                  </a>
+                </Link>
+              ))}
+
+              <LangSwitch className="header__lang" />
+            </div>
+          </div>
+        </div>
+      </header>
+
+      {isMobileMenuOpen && (
+        <MobileMenu onCloseClick={() => setIsMobileMenuOpen(false)} headerLinks={headerLinks} />
+      )}
+    </>
+  );
+}
+
+export default Header;
