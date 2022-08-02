8e51f5246bbbd3eb59fac759cfceb1af9604bf9f
Yasnovsky <andrey5.1997@mail.ru>
2022-08-02 10:46:11 +0500

    chore: #2tynw70: add button and fix key localization

--- components/HeroBlock/HeroBlock.tsx
+++ components/HeroBlock/HeroBlock.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'next-i18next';
+import SecondaryButton from '../SecondaryButton/SecondaryButton';
 
 function HeroBlock() {
   const { t } = useTranslation('heroBlock');
@@ -9,11 +10,14 @@ function HeroBlock() {
         <div className="hero-block__inner">
           <h1 className="title-type-1">
             {t('title')}
-            <span className="hero-block__gradient-title">{t('gradient-title')}</span>
+            <span className="hero-block__gradient-title">{t('gradientTitle')}</span>
           </h1>
           <div className="title-type-4 hero-block__description">{t('description')}</div>
 
-          <div>Кнопка</div>
+          <div className="hero-block__box-button">
+            <span>{t('buttonText')}</span>
+            <SecondaryButton className="hero-block__button" />
+          </div>
         </div>
         <div className="hero-block__video">
           <video src="/images/video.mp4" autoPlay loop playsInline muted>
