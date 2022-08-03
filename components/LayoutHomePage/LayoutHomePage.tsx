c5484ebf28397ae87ede17a04b3a5d432c8d0ef3
Yasnovsky <andrey5.1997@mail.ru>
2022-08-03 15:31:47 +0500

    feat(hero block): #2tynw70: add HeroBlock with localization

--- components/LayoutHomePage/LayoutHomePage.tsx
+++ components/LayoutHomePage/LayoutHomePage.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import HeroBlock from '../HeroBlock/HeroBlock';
 
 function LayoutHomePage({
   children,
@@ -11,6 +12,7 @@ function LayoutHomePage({
     <div className="layout-home-page">
       <div className="layout-home-page__header">
         <Header />
+        <HeroBlock />
       </div>
       <main className="container layout-home-page__wrapper">
         <div className="layout-home-page__nav">
