ea5c70d0130aaa3cba17f7b673f329a47a339e4e
MaxVaE <mvaliakine@tourmalinecore.com>
2022-08-10 10:09:24 +0500

    feat(workStructure): #2u9z43d: add images and caption

--- components/WorkStructure/WorkStructure.tsx
+++ components/WorkStructure/WorkStructure.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'next-i18next';
+import Image from 'next/image';
 
 function WorkStructure() {
   const { t } = useTranslation('workStructure');
@@ -8,6 +9,22 @@ function WorkStructure() {
       <h2 className="title-type-3">{t('title')}</h2>
       <div className="work-structure__subtitle">{t('subtitle')}</div>
 
+      <div className="work-structure__images">
+        <div className="ratio ratio--16x9 work-structure__first-image">
+          <Image
+            src="/images/good-code.png"
+            layout="fill"
+          />
+        </div>
+        <div className="ratio ratio--16x9 work-structure__second-image">
+          <Image
+            src="/images/good-work.png"
+            layout="fill"
+          />
+        </div>
+      </div>
+
+      <div className="caption work-structure__caption">{t('caption')}</div>
     </section>
   );
 }
