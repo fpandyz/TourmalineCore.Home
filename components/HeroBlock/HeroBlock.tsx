ece23f982019c0a6bb5d1c089659cd26494d138d
Yasnovsky <andrey5.1997@mail.ru>
2022-08-19 11:05:10 +0500

    chore(hero block): add new image for poster and background

--- components/HeroBlock/HeroBlock.tsx
+++ components/HeroBlock/HeroBlock.tsx
@@ -23,8 +23,7 @@ function HeroBlock({
 
         </div>
         <div className="hero-block__video">
-          <video poster="/images/tourmaline-core-poster.png" src="/images/video.mp4" autoPlay loop playsInline muted>
-            <source src="video.mp4" type="video/mp4" />
+          <video poster="/images/tourmaline-core-poster.webp" src="/images/video.mp4" autoPlay loop playsInline muted>
             <track kind="captions" />
           </video>
         </div>
