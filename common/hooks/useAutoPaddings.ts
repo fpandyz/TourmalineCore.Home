67f80ebf3d4ba9af6f5e00cf55c3885da631a461
Yasnovsky <andrey5.1997@mail.ru>
2022-08-26 15:21:40 +0500

    chore: #2vxmyj1: fix hooks

--- common/hooks/useAutoPaddings.ts
+++ common/hooks/useAutoPaddings.ts
@@ -1,28 +1,56 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import useDeviceSize from './useDeviceSize';
+
+function test() {
+  const [offset, setOffset] = useState(80);
+  const deviceSize = useDeviceSize();
+
+  useEffect(() => {
+    function getOffset() {
+      if (deviceSize.width >= 1920) {
+        return 200;
+      }
+      if (deviceSize.width >= 1366) {
+        return 176;
+      }
+      if (deviceSize.width >= 768) {
+        return 96;
+      }
+
+      return 80;
+    }
+
+    setOffset(getOffset());
+  }, [deviceSize.width]);
+
+  return offset;
+}
 
 function useAutoPaddings() {
-  useEffect(() => {
-    const screenHeight = document.documentElement.clientHeight;
+  const deviceSize = useDeviceSize();
 
+  const minPadding = test();
+
+  useEffect(() => {
     const allSection: NodeListOf<HTMLElement> = document.querySelectorAll('section[data-auto-padding]');
 
     allSection.forEach((section, index) => {
+      const screenHeight = document.documentElement.clientHeight;
       const elementHeight = section?.clientHeight;
-      const isNotMobileWidth = section?.clientWidth >= 756;
 
       const paddingCalculat = (screenHeight - elementHeight) / 2;
 
-      const paddingValue = paddingCalculat >= 0 ? Math.round(paddingCalculat) : 10;
+      const paddingValue = paddingCalculat > minPadding ? Math.round(paddingCalculat) : minPadding;
 
       if (index === allSection.length - 1) {
-        section.style.paddingTop = isNotMobileWidth ? `${paddingValue}px` : '80px';
+        section.style.paddingTop = `${paddingValue}px`;
         return;
       }
 
-      section.style.paddingTop = isNotMobileWidth ? `${paddingValue}px` : '80px';
-      section.style.paddingBottom = isNotMobileWidth ? `${paddingValue}px` : '80px';
+      section.style.paddingTop = `${paddingValue}px`;
+      section.style.paddingBottom = `${paddingValue}px`;
     });
-  }, []);
+  }, [deviceSize]);
 }
 
 export default useAutoPaddings;
