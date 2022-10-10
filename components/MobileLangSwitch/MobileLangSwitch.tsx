9fd1208290acc2dbb4993596309e4b8a943b1c67
MaxVaE <mvaliakine@tourmalinecore.com>
2022-10-10 18:31:24 +0500

    fix: change lang swich for mobile

--- components/MobileLangSwitch/MobileLangSwitch.tsx
+++ components/MobileLangSwitch/MobileLangSwitch.tsx
@@ -3,6 +3,7 @@ import {
 } from 'react';
 import clsx from 'clsx';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 import { languages } from '../../common/utils/consts/languages';
 import { DEFAULT_LOCALE } from '../../common/utils/consts/localization';
@@ -28,16 +29,24 @@ function MobileLangSwitch() {
             key={locale}
             className="mobile-lang-switch__option"
           >
-            <a
-              className={clsx(
-                'mobile-lang-switch__link',
-                { 'mobile-lang-switch__link--active': routerLocale === locale },
-              )}
-              href={router.pathname + locale}
-            >
-              {languages[locale].icon()}
-              {languages[locale].shortName}
-            </a>
+            <Link href={router.pathname} locale={locale}>
+              <a
+                role="presentation"
+                className={clsx(
+                  'mobile-lang-switch__link',
+                  { 'mobile-lang-switch__link--active': routerLocale === locale },
+                )}
+                onClick={(e) => {
+                  e.preventDefault();
+                  if (routerLocale !== locale) {
+                    window.open((e.target as HTMLAnchorElement).href, '_self');
+                  }
+                }}
+              >
+                {languages[locale].icon()}
+                {languages[locale].shortName}
+              </a>
+            </Link>
           </li>
         ))}
       </ul>
