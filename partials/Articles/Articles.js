4d0184982b703cd7afd4ebf142f32ef7c84ac124
Yasnovsky <andrey5.1997@mail.ru>
2022-12-12 11:20:56 +0500

    refactor: #37q39xq: add hover state, fix sort and remove superfluous code

--- partials/Articles/Articles.js
+++ partials/Articles/Articles.js
@@ -16,7 +16,7 @@ export default function Articles({
   const [sortingValue, setSortingValue] = useState(all);
 
   const articlesFilteredByLocale = articles.filter((article) => article.locale === router.locale)
-    .sort((adsfs, ssssb) => (adsfs.metadata.datePublication < ssssb.metadata.datePublication ? 1 : -1));
+    .sort((adsfs, ssssb) => (adsfs.metadata.datePublication > ssssb.metadata.datePublication ? 1 : -1));
 
   const article = Array.from(new Set(articlesFilteredByLocale.flatMap((item) => item.metadata.categories)));
   const sortElements = [all, ...article];
@@ -49,7 +49,6 @@ export default function Articles({
               {sortedArticles.map((folder) => (
                 <li key={folder.name} className="articles__item">
                   <ArticleLink
-                    articles={articles}
                     articleFolder={folder}
                     router={router}
                     locale={router.locale}
@@ -75,62 +74,40 @@ export default function Articles({
 }
 
 function ArticleLink({
-  articles,
   articleFolder = {},
   router,
 }) {
-  const availableLocales = router.locales.filter((locale) => locale !== router.locale);
-
   const image = getArticleUrl(articleFolder.name, articleFolder.locale);
 
   return (
-    <>
-      <Link href={`/articles/${articleFolder.metadata.slug}`} locale={router.locale}>
-        <a className="articles-link" title={articleFolder.metadata.description}>
-          <div className="articles-link__image">
-            <Image
-              src={articleFolder.metadata.previewImage ? `${image}/images/${articleFolder.metadata.previewImage}` : '/images/article-preview.webp'}
-              alt="preview photo"
-              layout="fill"
-            />
+    <Link href={`/articles/${articleFolder.metadata.slug}`} locale={router.locale}>
+      <a className="articles-link" title={articleFolder.metadata.description}>
+        <div className="articles-link__image">
+          <Image
+            src={articleFolder.metadata.previewImage ? `${image}/images/${articleFolder.metadata.previewImage}` : '/images/article-preview.webp'}
+            alt="preview photo"
+            layout="fill"
+          />
 
-            <ul className="articles-link__categories">
-              {articleFolder.metadata.categories.map((category) => (
-                <li
-                  key={category}
-                  className="articles-link__category"
-                >
-                  {category}
-                </li>
-              ))}
-            </ul>
+          <ul className="articles-link__categories">
+            {articleFolder.metadata.categories.map((category) => (
+              <li
+                key={category}
+                className="articles-link__category"
+              >
+                {category}
+              </li>
+            ))}
+          </ul>
 
-          </div>
+        </div>
 
-          <div className="articles-link__description">{articleFolder.metadata.description}</div>
-          <div className="articles-link__info">
-            <span>{articleFolder.metadata.author}</span>
-            <span>{articleFolder.metadata.datePublication}</span>
-          </div>
-        </a>
-      </Link>
-
-      {/* {' '}
-      (
-      {availableLocales.map((locale) => {
-        if (articles.find((article) => article.locale === locale)) {
-          return (
-            <Link key={locale} href={`/articles/${articleFolder.metadata.slug}`} locale={locale}>
-              <a className="articles__link">
-                {locale.toUpperCase()}
-              </a>
-            </Link>
-          );
-        }
-
-        return null;
-      })}
-      ) */}
-    </>
+        <div className="articles-link__description">{articleFolder.metadata.description}</div>
+        <div className="articles-link__info">
+          <span>{articleFolder.metadata.author}</span>
+          <span>{articleFolder.metadata.datePublication}</span>
+        </div>
+      </a>
+    </Link>
   );
 }
