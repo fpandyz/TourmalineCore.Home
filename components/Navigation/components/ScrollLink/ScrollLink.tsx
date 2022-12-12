cd7eb64b9c86cedec3cf13fce8950633dba6c774
Yasnovsky <andrey5.1997@mail.ru>
2022-12-12 17:44:01 +0500

    feat(navigate): #382r7fy: add anchor links and remove unnecessary npm package

--- components/Navigation/components/ScrollLink/ScrollLink.tsx
+++ components/Navigation/components/ScrollLink/ScrollLink.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-scroll';
 import { clsx } from 'clsx';
-import { useState } from 'react';
 import isChineseLanguage from '../../../../common/utils/isChineseLanguage';
 
 function ScrollLink({
@@ -8,44 +7,33 @@ function ScrollLink({
   text,
   to,
   setIsSeeNavigation,
-  scrollTo,
 }: {
   index: number;
   text: string;
   to: string;
   setIsSeeNavigation: (value: boolean) => unknown;
-  scrollTo: () => unknown;
 }) {
-  const [isActive, setIsActive] = useState(false);
-
   return (
-    <>
-      <Link
-        spy
-        to={to}
-        onSetActive={() => {
-          setIsSeeNavigation(true);
-          setIsActive(true);
-        }}
-        onSetInactive={() => {
-          if (index === 0) {
-            setIsSeeNavigation(false);
-          }
-
-          setIsActive(false);
-        }}
-      />
-      <button
-        type="button"
-        className={clsx('navigation__link', {
-          'navigation__link--active': isActive,
-          'navigation__link--zh': isChineseLanguage(),
-        })}
-        onClick={scrollTo}
-      >
-        {text}
-      </button>
-    </>
+    <Link
+      spy
+      to={to}
+      className={clsx('navigation__link', {
+        'navigation__link--zh': isChineseLanguage(),
+      })}
+      activeClass="navigation__link--active"
+      hashSpy
+      smooth
+      onSetActive={() => {
+        setIsSeeNavigation(true);
+      }}
+      onSetInactive={() => {
+        if (index === 0) {
+          setIsSeeNavigation(false);
+        }
+      }}
+    >
+      {text}
+    </Link>
   );
 }
 
