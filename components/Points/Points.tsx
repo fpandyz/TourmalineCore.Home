19fbec6a9dbb58b6927171d0d4ba990d018f4aba
Dmitriy Myakotin <demon4301@gmail.com>
2024-01-19 09:12:27 +0500

    refactor: #8693jtqq7 : took out a constant variable

--- components/Points/Points.tsx
+++ components/Points/Points.tsx
@@ -1,21 +1,19 @@
 import { useRouter } from 'next/router';
 
+const POINTS_LIST = ['Доработаем существующую систему согласно требованиям', 'Увеличим производительность системы', 'Разработаем клиентскую часть с нуля'];
+const POINTS_TITLE = 'Создаем адаптивные кроссбраузерные интерфейсы для любых направлений бизнеса';
+
 // TODO add data from json
 export default function Points() {
-  const pointsList = ['Доработаем существующую систему согласно требованиям', 'Увеличим производительность системы', 'Разработаем клиентскую часть с нуля'];
   const { pathname } = useRouter();
 
   return (
     <section className="points">
       <div className="container points__wrapper">
         <div className="points__inner">
-          <h3 className="title-techology points__title">
-            Создаем адаптивные
-            кроссбраузерные интерфейсы
-            для любых направлений бизнеса
-          </h3>
+          <h3 className="title-techology points__title">{POINTS_TITLE}</h3>
           <ul className="points__list">
-            {pointsList.map((text) => (
+            {POINTS_LIST.map((text) => (
               <li
                 key={text}
                 className="points__item"
