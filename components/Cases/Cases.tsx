e37190d7325c6f0c3146b1474d470c7a3256d6f6
Dmitriy Myakotin <demon4301@gmail.com>
2024-01-23 09:31:27 +0500

    chore: #8693k2x99: add property link and fixed style

--- components/Cases/Cases.tsx
+++ components/Cases/Cases.tsx
@@ -15,52 +15,62 @@ const CASES_LIST = [
     title: 'Tourmanique',
     description: 'Cервис определения уникальности фотографий',
     icon: <IconCamera />,
+    link: '',
   },
   {
     title: 'Sine and Clever',
     description: 'Cистема управления персоналом и расчета бонусов сотрудников',
     icon: <IconPeople />,
+    link: '',
   },
   {
     title: 'Real Estate',
     description: 'Сервис для работы с объявлениями и их выгрузки на сайты недвижимости',
     icon: <IconMonitor />,
+    link: '',
   },
   {
     title: 'Trade Hack',
     description: 'Сервис сбора аналитики  по товарам на площадке маркетплейса Wildberries',
     icon: <IconChart />,
+    link: '',
   },
   {
     title: 'Fair Action',
     description: 'Сайт для австралийской благотворительной организации',
     icon: <IconHeart />,
+    link: '',
   },
   {
     title: 'Inner Circle',
     description: 'Сервис для работы с объявлениями и их выгрузки  на сайты недвижимости',
     icon: <IconAdvertisement />,
+    link: '',
   },
   {
     title: 'Best By',
     description: 'Сервис для отслеживания срока годности продукции',
     icon: <IconIceCream />,
+    link: '',
   },
 ];
 
 export default function Cases() {
   return (
-    <div className="container cases__wrapper">
-      <div className="cases__inner">
-        <h3 className="title-technology cases__title">{CASES_TITLE}</h3>
-        {CASES_LIST.map((item) => (
-          <CasesCard
-            title={item.title}
-            description={item.description}
-            icon={item.icon}
-          />
-        ))}
+    <section className="cases">
+      <div className="container cases__wrapper">
+        <div className="cases__inner">
+          <h3 className="title-technology cases__title">{CASES_TITLE}</h3>
+          {CASES_LIST.map((item) => (
+            <CasesCard
+              title={item.title}
+              description={item.description}
+              icon={item.icon}
+              link={item.link}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
--- components/CasesCard/CasesCard.tsx
+++ components/CasesCard/CasesCard.tsx
@@ -6,16 +6,18 @@ export default function CasesCard(
     title,
     description,
     icon,
+    link,
   }:{
     title: string;
     description: string;
     icon: JSX.Element;
+    link: string
   },
 ) {
   const { pathname } = useRouter();
 
   return (
-    <a className={`cases-card cases-card--${pathname.slice(1)}`}>
+    <a href={link} className={`cases-card cases-card--${pathname.slice(1)}`}>
       <div className="cases-card__inner">
         <div className="cases-card__icon">{icon}</div>
         <h3 className="cases-card__title">{title}</h3>
