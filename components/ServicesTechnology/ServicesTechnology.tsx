ab63ef525416074d7cb6c9986cecd02896096f28
Dmitriy Myakotin <demon4301@gmail.com>
2024-01-25 10:17:29 +0500

    chore: #8693kybrm: add property link for services technology list

--- components/ServicesTechnology/ServicesTechnology.tsx
+++ components/ServicesTechnology/ServicesTechnology.tsx
@@ -5,18 +5,22 @@ const SERVICES_TECHNOLOGY_LIST = [
   {
     title: 'UI/UX дизайн',
     description: 'Разработаем внешний вид системы',
+    link: '',
   },
   {
     title: 'Backend',
     description: 'Создадим  инфраструктуру и серверную часть ',
+    link: '',
   },
   {
     title: 'ML',
     description: 'Добавим искусственный интеллект ',
+    link: '',
   },
   {
     title: 'QA',
     description: 'Протестируем систему',
+    link: '',
   },
 ];
 
@@ -27,9 +31,9 @@ export default function ServicesTechnology() {
         <div className="services-technology__inner">
           <h3 className="title-technology-type-1 services-technology__title">{SERVICES_TECHNOLOGY_TITLE}</h3>
           <ul className="services-technology__list">
-            {SERVICES_TECHNOLOGY_LIST.map(({ title, description }) => (
+            {SERVICES_TECHNOLOGY_LIST.map(({ title, description, link }) => (
               <li key={title} className="services-technology__item">
-                <ServicesTechnologyCard title={title} description={description} />
+                <ServicesTechnologyCard title={title} description={description} link={link} />
               </li>
             ))}
           </ul>
--- components/ServicesTechnology/ServicesTechnologyCard/ServicesTechnologyCard.tsx
+++ components/ServicesTechnology/ServicesTechnologyCard/ServicesTechnologyCard.tsx
@@ -4,20 +4,22 @@ export default function ServicesTechnologyCard(
   {
     title,
     description,
+    link,
   }:
   {
     title: string;
     description: string;
+    link: string
   },
 ) {
   const { pathname } = useRouter();
 
   return (
-    <div className={`services-technology-card services-technology-card--${pathname.slice(1)}`}>
+    <a href={link} className={`services-technology-card services-technology-card--${pathname.slice(1)}`}>
       <div className="services-technology-card__inner">
         <h3 className="title-technology-type-2 services-technology-card__title">{title}</h3>
         <span className="services-technology-card__description">{description}</span>
       </div>
-    </div>
+    </a>
   );
 }
