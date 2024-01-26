74a3958a66c64fa5db9a39dea5a9d2871184a9ea
Dmitriy Myakotin <demon4301@gmail.com>
2024-01-26 09:07:40 +0500

    chore: #8693kyaty: add test translation for payment block

--- components/Payment/PaymentList/PaymentList.tsx
+++ components/Payment/PaymentList/PaymentList.tsx
@@ -1,43 +1,50 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+import { useTranslation } from 'next-i18next';
 import NamedList from '../../NamedList/NamedList';
 
-const PAYMENT_LIST = [
-  {
-    paymentListTitle: 'Почасовая оплата',
-    paymentListData: [
-      {
-        namedListTitle: 'Для чего',
-        namedListData: ['Разработка крупных систем и R&D проектов'],
-      },
-      {
-        namedListTitle: 'Условия',
-        namedListData: ['Фиксируем не сроки, а желаемый результат и ставки разработчиков'],
-      },
-    ],
-  },
-  {
-    paymentListTitle: 'Фиксированная стоимость',
-    paymentListData: [
-      {
-        namedListTitle: 'Для чего',
-        namedListData: ['Небольшие и предсказуемые проекты'],
-      },
-      {
-        namedListTitle: 'Условия',
-        namedListData: ['Фиксируем сроки, стоимость и объем работ'],
-      },
-    ],
-  },
-];
+// const PAYMENT_LIST = [
+//   {
+//     mainTitle: 'Почасовая оплата',
+//     mainData: [
+//       {
+//         title: 'Для чего',
+//         data: ['Разработка крупных систем и R&D проектов'],
+//       },
+//       {
+//         namedListTitle: 'Условия',
+//         namedListData: ['Фиксируем не сроки, а желаемый результат и ставки разработчиков'],
+//       },
+//     ],
+//   },
+//   {
+//     paymentListTitle: 'Фиксированная стоимость',
+//     paymentListData: [
+//       {
+//         namedListTitle: 'Для чего',
+//         namedListData: ['Небольшие и предсказуемые проекты'],
+//       },
+//       {
+//         namedListTitle: 'Условия',
+//         namedListData: ['Фиксируем сроки, стоимость и объем работ'],
+//       },
+//     ],
+//   },
+// ];
 
 export default function PaymentList() {
+  const { t } = useTranslation('payment-frontend');
+
+  const list: any[] = t('list', { returnObjects: true });
+
   return (
     <ul className="payment-list">
-      {PAYMENT_LIST.map(({ paymentListTitle, paymentListData }) => (
-        <li key={paymentListTitle} className="payment-list__item">
-          <h4 className="title-technology-type-2 payment-list__title">{paymentListTitle}</h4>
+      {list.map(({ mainTitle, mainData }) => (
+        <li key={mainTitle} className="payment-list__item">
+          <h4 className="title-technology-type-2 payment-list__title">{mainTitle}</h4>
           <div className="payment-list__inner">
-            {paymentListData.map(({ namedListTitle, namedListData }) => (
-              <NamedList title={namedListTitle} data={namedListData} />
+            {mainData.map(({ title, data }) => (
+              <NamedList title={title} data={data} />
             ))}
           </div>
         </li>
