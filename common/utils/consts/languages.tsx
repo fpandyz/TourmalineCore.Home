ccaf7550f4c041d5b6df923bf0826d8be5472a0b
Dmitriy Myakotin <demon4301@gmail.com>
2024-06-13 13:34:00 +0500

    chore: comment language

--- common/utils/consts/languages.tsx
+++ common/utils/consts/languages.tsx
@@ -1,4 +1,4 @@
-import IconRussianFlag from '../../../icons/flag-for-russia.svg';
+// import IconRussianFlag from '../../../icons/flag-for-russia.svg';
 import IconUSAFlag from '../../../icons/flag-for-united-states.svg';
 import IconChinaFlag from '../../../icons/flag-for-china.svg';
 
@@ -16,11 +16,11 @@ export const languages: Languages = {
     shortName: 'en',
     icon: () => <IconUSAFlag className="lang-switch__flag" />,
   },
-  ru: {
-    name: 'Русский',
-    shortName: 'рус',
-    icon: () => <IconRussianFlag className="lang-switch__flag" />,
-  },
+  // ru: {
+  //   name: 'Русский',
+  //   shortName: 'рус',
+  //   icon: () => <IconRussianFlag className="lang-switch__flag" />,
+  // },
   zh: {
     name: '中文',
     shortName: '中文',
