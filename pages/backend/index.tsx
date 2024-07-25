8c8fd1ee0ea7ba066b3f7f381c6475300f286ea9
Artem Sheptunov <106321977+Infindery@users.noreply.github.com>
2024-07-25 09:51:05 +0500

    feat: add Tasks component for Backend page

--- pages/backend/index.tsx
+++ pages/backend/index.tsx
@@ -5,6 +5,7 @@ import Layout from '../../components/Layout/Layout';
 import PageHead from '../../components/PageHead/PageHead';
 import HeroBlockTechnology from '../../components/HeroBlockTechnology/HeroBlockTechnology';
 import Points from '../../components/Points/Points';
+import Tasks from '../../components/Tasks/Tasks';
 
 export default function BackendPage() {
   const { t } = useTranslation('common');
@@ -28,6 +29,7 @@ export default function BackendPage() {
           <HeroBlockTechnology />
           <Points />
         </div>
+        <Tasks />
       </Layout>
     </>
   );
