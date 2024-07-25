cc255a292a15e91f5016ccf838eae35ed2bf569c
Artem Sheptunov <106321977+Infindery@users.noreply.github.com>
2024-07-25 10:47:34 +0500

    feat: add ServicesTechnologyBackend component for Backend page

--- pages/backend/index.tsx
+++ pages/backend/index.tsx
@@ -10,6 +10,7 @@ import Cta from '../../components/Cta/Cta';
 import Stack from '../../components/Stack/Stack';
 import Payment from '../../components/Payment/Payment';
 import Cooperation from '../../components/Cooperation/Cooperation';
+import ServicesTechnology from '../../components/ServicesTechnology/ServicesTechnology';
 
 export default function BackendPage() {
   const { t } = useTranslation('common');
@@ -38,6 +39,7 @@ export default function BackendPage() {
         <Stack />
         <Payment />
         <Cooperation />
+        <ServicesTechnology />
       </Layout>
     </>
   );
