27fb438e2652496c181c8256c633fef16649ec11
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-12-16 12:25:33 +0500

    feat: add Teams page markup and styles

---
+++ pages/teams/index.tsx
@@ -0,0 +1,80 @@
+import { GetServerSideProps } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'next-i18next';
+import Layout from '../../components/Layout/Layout';
+import PageHead from '../../components/PageHead/PageHead';
+import HeroBlockTechnology from '../../components/HeroBlockTechnology/HeroBlockTechnology';
+import Points from '../../components/Points/Points';
+import Tasks from '../../components/Tasks/Tasks';
+import Stack from '../../components/Stack/Stack';
+import Cases from '../../components/Cases/Cases';
+import Stages from '../../components/Stages/Stages';
+import ServicesTechnology from '../../components/ServicesTechnology/ServicesTechnology';
+import Cta from '../../components/Cta/Cta';
+import Cooperation from '../../components/Cooperation/Cooperation';
+import Payment from '../../components/Payment/Payment';
+import FormBlock from '../../components/FormBlock/FormBlock';
+import { TechnologyPageAnchorLink } from '../../common/utils/consts/technology-anchor-link';
+
+export default function TeamsPage() {
+  const { t } = useTranslation('common');
+
+  return (
+    <>
+      <PageHead
+        seoData={{
+          seo: {
+            title: t('title'),
+            description: t('description'),
+          },
+          keywords: t('keywords'),
+          metaTags: [],
+          structuredData: '',
+          additionalCode: '',
+        }}
+      />
+      <Layout mainClassName="teams">
+        <div className="teams__hero-block-container">
+          <HeroBlockTechnology />
+          <Points />
+        </div>
+        <Tasks />
+        <Stack />
+        <Cases />
+        <Stages />
+        <ServicesTechnology />
+        <Cta />
+        <Cooperation />
+        <Payment />
+        <FormBlock
+          id={TechnologyPageAnchorLink.Contact}
+          buttonClassName="teams__form-button"
+        />
+      </Layout>
+    </>
+  );
+}
+
+export const getStaticProps: GetServerSideProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale as string, [
+      'common',
+      'footer',
+      'heroBlockHomePage',
+      'cookie',
+      'form',
+      'formBlock',
+      'discussion',
+      'paymentTeams',
+      'cta',
+      'cooperationTeams',
+      'heroTeams',
+      'pointsTeams',
+      'tasksTeams',
+      'stackTeams',
+      'casesTeams',
+      'stagesTeams',
+      'servicesTechnologyTeams',
+    ])),
+  },
+});
