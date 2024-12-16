cfc303dc5520a7e495c296139668a2a9820ae5e3
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-12-16 12:43:00 +0500

    refactor: add Teams page modifications for StagesList component styles and add slicePathname config

--- components/Stages/components/StagesList/StagesList.tsx
+++ components/Stages/components/StagesList/StagesList.tsx
@@ -1,3 +1,4 @@
+import usePath from '../../../../common/hooks/usePath';
 import { TStagesList } from '../../types';
 
 export default function StagesList({
@@ -5,6 +6,8 @@ export default function StagesList({
 }:{
   list: TStagesList
 }) {
+  const { slicePathname } = usePath();
+
   return (
     <ul className="stages-list">
       {list.map(({ title, description }, index) => (
@@ -12,10 +15,10 @@ export default function StagesList({
           key={title}
           className="stages-list__item"
         >
-          <div className="stages-list__number">{index + 1}</div>
+          <div className={`stages-list__number stages-list__number--${slicePathname}`}>{index + 1}</div>
           <div className="stages-list__inner">
             <span className="title-technology-type-3 stages-list-title">{title}</span>
-            <span className="stages-list__description">{description}</span>
+            <span className={`stages-list__description stages-list__description--${slicePathname}`}>{description}</span>
           </div>
         </li>
       ))}
