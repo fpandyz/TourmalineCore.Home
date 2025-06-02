11f8b90f198b5939cbca2f492f307dac0073a8ac
Ekaterina <echekina@gmail.com>
2025-06-02 09:56:20 +0500

    chore: styling

--- components/redesign/ImageRedesign/ImageRedesign.tsx
+++ components/redesign/ImageRedesign/ImageRedesign.tsx
@@ -1,14 +1,19 @@
 import { useTranslation } from 'next-i18next';
+import Image from 'next/image';
 
 export function ImageRedesign() {
   const { t } = useTranslation('imageRedesign');
 
   return (
     <div className="image-redesign container-redesign">
-      <img
-        src={t('imageUrl')}
-        alt=""
-      />
+      <div className="image-redesign__container">
+       <Image
+          src={t('imageUrl')}
+          layout="fill"
+          priority
+          alt=""
+        />
+      </div>
     </div>
 
   );
