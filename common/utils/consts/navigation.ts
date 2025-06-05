382004d58053709f70674d0d9989c7cee3421f94
Artem Sheptunov <sheptunov-aa@mail.ru>
2025-06-05 07:57:45 +0500

    refactor: remove redundant const

--- common/utils/consts/navigation.ts
+++
@@ -1,9 +0,0 @@
-export enum NavigationLinks {
-  services = 'services',
-  skills = 'skills',
-  experience = 'experience',
-  approach = 'approach',
-  contact = 'contact',
-}
-
-export const navigationLinks = Object.values(NavigationLinks);
