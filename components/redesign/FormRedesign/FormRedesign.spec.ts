71e1708dc2510e3e95cd07c6f1faa36c42260238
Ekaterina <echekina@gmail.com>
2025-06-24 14:54:22 +0500

    test: add test for FormRedesign

---
+++ components/redesign/FormRedesign/FormRedesign.spec.ts
@@ -0,0 +1,63 @@
+import { Page } from '@playwright/test';
+import { test, expect, CustomTestFixtures } from '../../../playwright-tests/custom-test';
+import { Breakpoint, BreakpointName } from '../../../common/utils/enum';
+
+const TEST_ID = `form`;
+
+test.describe(`FormRedesignTests`, () => {
+  test.beforeEach(async ({
+    goto,
+    apiImageMock,
+    hideCookie,
+  }) => {
+    await apiImageMock();
+
+    await goto();
+
+    await hideCookie();
+  });
+
+  test(`MobileTest`, mobileTest);
+
+  test(`DesktopTest`, desktopTest);
+});
+
+async function mobileTest({
+  page,
+  setViewportSize,
+}: {
+  page: Page;
+  setViewportSize: CustomTestFixtures['setViewportSize'];
+}) {
+  await setViewportSize();
+
+  await expect(getFormByTestId({
+    page,
+  }))
+    .toHaveScreenshot(`${TEST_ID}-${BreakpointName.MOBILE}.png`);
+}
+
+async function desktopTest({
+  page,
+  setViewportSize,
+}: {
+  page: Page;
+  setViewportSize: CustomTestFixtures['setViewportSize'];
+}) {
+  await setViewportSize({
+    width: Breakpoint.DESKTOP,
+  });
+
+  await expect(getFormByTestId({
+    page,
+  }))
+    .toHaveScreenshot(`${TEST_ID}-${BreakpointName.DESKTOP}.png`);
+}
+
+function getFormByTestId({
+  page,
+}: {
+  page: Page;
+}) {
+  return page.getByTestId(TEST_ID);
+}
