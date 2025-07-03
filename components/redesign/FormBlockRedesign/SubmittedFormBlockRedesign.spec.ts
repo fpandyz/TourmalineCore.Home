df13d738d2d2c6bf55251eb2efc6520a10c4f6d7
Ekaterina <echekina@gmail.com>
2025-07-03 10:50:15 +0500

    test: refactor according to new custom-test approach

--- components/redesign/FormBlockRedesign/SubmittedFormBlockRedesign.spec.ts
+++ components/redesign/FormBlockRedesign/SubmittedFormBlockRedesign.spec.ts
@@ -1,77 +1,29 @@
-import { Page } from '@playwright/test';
-import { test, expect, CustomTestFixtures } from '../../../playwright-tests/custom-test';
-import { ComponentName } from '../../../common/enums/component-name';
-import { Breakpoint, BreakpointName } from '../../../common/enums/breakpoint';
+import { test } from '../../../playwright-tests/custom-test';
+import { BREAKPOINTS } from '../../../playwright-tests/constants/breakpoints';
+import { ComponentName } from '../../../common/enums';
 
 const TEST_ID = `submitted-form-block`;
 
-test.describe(`SubmittedFormBlockRedesignTests`, () => {
+test.describe(`SubmittedFormBlockTests`, () => {
   test.beforeEach(async ({
     goToComponentsPage,
   }) => {
     await goToComponentsPage(ComponentName.SUBMITTED_FORM_BLOCK);
   });
 
-  test(`MobileTest`, mobileTest);
-
-  test(`TabletTest`, tabletTest);
-
-  test(`DesktopTest`, desktopTest);
+  for (const {
+    name,
+    breakpoint,
+    breakpointName,
+  } of BREAKPOINTS) {
+    test(name, async ({
+      testScreenshotAtBreakpoint,
+    }) => {
+      await testScreenshotAtBreakpoint({
+        testId: TEST_ID,
+        breakpoint,
+        breakpointName,
+      });
+    });
+  }
 });
-
-async function mobileTest({
-  page,
-  setViewportSize,
-}: {
-  page: Page;
-  setViewportSize: CustomTestFixtures['setViewportSize'];
-}) {
-  await setViewportSize();
-
-  await expect(getFormByTestId({
-    page,
-  }))
-    .toHaveScreenshot(`${TEST_ID}-${BreakpointName.MOBILE}.png`);
-}
-
-async function tabletTest({
-  page,
-  setViewportSize,
-}: {
-  page: Page;
-  setViewportSize: CustomTestFixtures['setViewportSize'];
-}) {
-  await setViewportSize({
-    width: Breakpoint.TABLET,
-  });
-
-  await expect(getFormByTestId({
-    page,
-  }))
-    .toHaveScreenshot(`${TEST_ID}-${BreakpointName.TABLET}.png`);
-}
-
-async function desktopTest({
-  page,
-  setViewportSize,
-}: {
-  page: Page;
-  setViewportSize: CustomTestFixtures['setViewportSize'];
-}) {
-  await setViewportSize({
-    width: Breakpoint.DESKTOP,
-  });
-
-  await expect(getFormByTestId({
-    page,
-  }))
-    .toHaveScreenshot(`${TEST_ID}-${BreakpointName.DESKTOP}.png`);
-}
-
-function getFormByTestId({
-  page,
-}: {
-  page: Page;
-}) {
-  return page.getByTestId(TEST_ID);
-}
