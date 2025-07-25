83d6de95740f26b43c067daf0131fdb3dda66881
Artem Sheptunov <sheptunov-aa@mail.ru>
2025-07-25 06:26:49 +0500

    feat: add HeaderNavigationRedesign component

---
+++ components/redesign/HeaderRedesign/components/HeaderNavigation/HeaderNavigation.tsx
@@ -0,0 +1,13 @@
+import { Accordion } from "../../../Accordion/Accordion";
+
+export function HeaderNavigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Accordion />
+        </li>
+      </ul>
+    </nav>
+  );
+}
