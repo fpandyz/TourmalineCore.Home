ae178005903a5d09d7fa8b651ceebc578a90eea1
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-08-08 13:06:24 +0500

    chore: hide FormBlock

--- pages/frontend-team/index.tsx
+++ pages/frontend-team/index.tsx
@@ -10,8 +10,6 @@ import { Tasks } from '../../components/Tasks/Tasks';
 import { Stack } from '../../components/Stack/Stack';
 import { Cases } from '../../components/Cases/Cases';
 import { Stages } from '../../components/Stages/Stages';
-import { FormBlock } from '../../components/FormBlock/FormBlock';
-import { TechnologyPageAnchorLink } from '../../common/enums';
 
 export default function FrontendTeamPage() {
   const {
@@ -46,10 +44,10 @@ export default function FrontendTeamPage() {
         <Stages />
         <Stack />
         <ServicesTechnology />
-        <FormBlock
+        {/* <FormBlock
           id={TechnologyPageAnchorLink.Contact}
           buttonClassName="frontend-team__form-button"
-        />
+        /> */}
       </Layout>
     </>
   );
