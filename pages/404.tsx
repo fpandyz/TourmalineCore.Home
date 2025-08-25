e58b9ce69aa51bb08ffaa8a31359a27140332cc6
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-08-25 11:02:38 +0500

    chore: remove layout in 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,42 +1,19 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { GetServerSideProps } from 'next';
 import { useTranslation } from 'next-i18next';
 
-import { LayoutRedesign } from '../components/redesign/LayoutRedesign/LayoutRedesign';
+import { GetServerSideProps } from 'next';
 import { CustomError } from '../components/redesign/CustomError/CustomError';
-import { HeaderRedesignProps } from '../common/types';
 
 export default function Custom404() {
   const {
     t,
   } = useTranslation(`pageNotFound`);
 
-  const {
-    t: headerTranslations,
-  } = useTranslation(`headerRedesign`);
-
-  const headerContent: HeaderRedesignProps = {
-    navigationLists: headerTranslations(`navigationLists`, {
-      returnObjects: true,
-    }),
-    button: headerTranslations(`button`, {
-      returnObjects: true,
-    }),
-    email: headerTranslations(`email`, {
-      returnObjects: true,
-    }),
-    socialLinks: headerTranslations(`socialLinks`, {
-      returnObjects: true,
-    }),
-  };
-
   return (
-    <LayoutRedesign headerContent={headerContent}>
-      <CustomError
-        statusCode={404}
-        message={t(`message`)}
-      />
-    </LayoutRedesign>
+    <CustomError
+      statusCode={404}
+      message={t(`message`)}
+    />
   );
 }
 
@@ -44,14 +21,6 @@ export const getStaticProps: GetServerSideProps = async ({
   locale,
 }) => ({
   props: {
-    ...(await serverSideTranslations(locale as string, [
-      `common`,
-      `articles`,
-      `footerRedesign`,
-      `pageNotFound`,
-      `cookie`,
-      `formBlockRedesign`,
-      `headerRedesign`,
-    ])),
+    ...(await serverSideTranslations(locale as string, [`pageNotFound`, `cookie`])),
   },
 });
